ba32185a1a3bc76c8c03aee76bac3cf6cdd4e750
Wbec <wbeckett@uvic.ca>
2017-02-28 15:39:15 -0800

    Update quiter.py

--- quiter.py
+++ quiter.py
@@ -1,3 +1,6 @@
+#The quit error is designed to allow the user to quit a program from any input prompt
+#it is less efficent than having each prompt have a quit option,
+#using this error quitting can be handled by a single try-except block in the main method
 class QuitError (Exception):
     "Error allowing for the exit of a program by a user."
     pass
